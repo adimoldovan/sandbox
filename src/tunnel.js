534845a440510828159bb7707a97a2594c1ef451
Adrian Moldovan <adim.moldovan@gmail.com>
2021-04-09 13:12:07 +0300

    Make the script to actually do something

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
+
 const localtunnel = require('localtunnel');
 const fs = require('fs');
 
-const tunnel = await localtunnel();
-console.log(`Opened tunnel for '${tunnel.clientId}', url: '${tunnel.url}'`);
-fs.writeFileSync('tunnel-url', tunnel.url);
-process.send('ready');
+(async () => {
+    const tunnel = await localtunnel();
+    console.log(`Opened tunnel for '${tunnel.clientId}', url: '${tunnel.url}'`);
+    fs.writeFileSync('tunnel-url', tunnel.url);
+    console.log("wrote url to file");
+})();
