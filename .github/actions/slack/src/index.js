1303152751350bbcdddd667563e6b62e6fcadb0b
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2022-07-29 14:17:23 +0300

    Get run context

--- .github/actions/slack/src/index.js
+++ .github/actions/slack/src/index.js
@@ -1,8 +1,14 @@
 const { setFailed, getInput } = require( '@actions/core' );
-const { context } = require( '@actions/github' );
+const { context, getOctokit } = require( '@actions/github' );
 const { WebClient, retryPolicies, LogLevel } = require( '@slack/web-api' );
 
 ( async function main() {
+	const ghToken = getInput( 'github_token' );
+	if ( ! ghToken ) {
+		setFailed( 'main: Input `github_token` is required' );
+		return;
+	}
+
 	const token = getInput( 'slack_token' );
 	if ( ! token ) {
 		setFailed( 'Input `slack_token` is required' );
@@ -16,6 +22,16 @@ const { WebClient, retryPolicies, LogLevel } = require( '@slack/web-api' );
 	}
 
 	process.stdout.write(JSON.stringify(context));
+
+	const octokit = new getOctokit( ghToken );
+
+	const run = octokit.rest.actions.getWorkflowRun({
+		owner: context.payload.repository.owner.login,
+		repo: context.payload.repository.name,
+		run_id: context.runId,
+	  });
+
+	process.stdout.write(JSON.stringify(run));
 
 	const client = new WebClient( token, {
 		retryConfig: retryPolicies.rapidRetryPolicy,
