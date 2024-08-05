6f0fd26dbc8cc29a30caf0d255d5d6047b910752
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-08-05 18:30:26 +0300

    Download last run

--- src/pw-test.spec.js
+++ src/pw-test.spec.js
@@ -1,8 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test one', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.getByRole('link', { name: 'Get started' }).click();
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
   await expect(1).toBe(2);
+});
+
+test('test two', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
