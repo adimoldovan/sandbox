4b8c4f6be0faffe38d41676cc00bd4ec225b25b6
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-08-05 18:20:54 +0300

    Add pw tests

---
+++ src/pw-test.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(1).toBe(2);
+});
